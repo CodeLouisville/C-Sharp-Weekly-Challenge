78da6fca069bb335c7ea1ca7f21ad5fc29c4447e
chuxbraganza <cbraganza@gmail.com>
2016-09-04 12:46:11 -0400

    Completed Week 2 Challenges.

--- Challenges/Week2/CodeLou.CSharp.Week2.Challenge/CodeLou.CSharp.Week2.Challenge/Program.cs
+++ Challenges/Week2/CodeLou.CSharp.Week2.Challenge/CodeLou.CSharp.Week2.Challenge/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 namespace CodeLou.CSharp.Week2.Challenge
 {
@@ -6,6 +6,7 @@ namespace CodeLou.CSharp.Week2.Challenge
     {
         static void Main(string[] args)
         {
+            int secondsBeforeLiftOff = 0;
             // Task 1:
             // The Console class is in the System namespace in the .Net framework. Add a "using" statement
             // to the top of this file to import the System namespace so that the C# compiler knows where
@@ -16,7 +17,7 @@ namespace CodeLou.CSharp.Week2.Challenge
             // Hint: In Visual Studio, you can build the project by selecting "Build Solution" from the
             //       build menu, or by pressing <Ctrl>+<Shift>+B.
             Console.WriteLine("Welcome to the Code Louisville C# week 2 code challenge!");
-            Console.WriteLine("Press <Enter> to begin...")
+            Console.WriteLine("Press <Enter> to begin...");
             Console.ReadLine();
 
             Console.WriteLine("This is the launch application for the first human mission to Mars.");
@@ -27,6 +28,7 @@ namespace CodeLou.CSharp.Week2.Challenge
             // Hint: You should use another method of the Console class and store the output into a
             //       variable to use later.
 
+             Int32.TryParse(Console.ReadLine(), out secondsBeforeLiftOff);
 
             // Task 4:
             // Write a condition to test whether the number that they entered is less than or equal to zero.
@@ -34,7 +36,10 @@ namespace CodeLou.CSharp.Week2.Challenge
             // If the result is true, write "Please enter a positive number." to the console.
             // Hint: The input that you captured is currently a string type. You will have to "parse" it
             //       as a different type in order to pass it to the IsLessThanOrEqualToZero function.
-
+            if (IsLessThanOrEqualToZero(secondsBeforeLiftOff))
+            {
+                Console.WriteLine("Please enter a positive number");
+            }
 
             // Task 5:
             // Add an "else" block to the condition from Task 4. This should be run in the case that the
@@ -44,6 +49,17 @@ namespace CodeLou.CSharp.Week2.Challenge
             //       "for". You can choose whichever you'd like to solve the task. The Microsoft
             //       Developer Network (MSDN) website contains all of the documentation for C#. If you want
             //       to learn more about loops, visit https://msdn.microsoft.com/en-us/library/32dbftby.aspx.
+            else
+            {
+                while (secondsBeforeLiftOff > -1)
+                {
+                    Console.WriteLine(secondsBeforeLiftOff + "...");
+                    secondsBeforeLiftOff-=1;
+                }
+                Console.WriteLine("LIFTOFF!");
+
+            }
+
 
             Console.WriteLine("Press <Enter> to exit...");
             Console.ReadLine();
