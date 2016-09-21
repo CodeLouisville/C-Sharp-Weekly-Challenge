2f978e8ec9d3581d83b479f80ac1a2291aba7a34
Steven Heigl <sheigl@gmail.com>
2016-09-21 07:45:54 -0400

    Added repository class and moved methods to it. Added comments etc.

--- Challenges/Week4/CodeLou.CSharp.Week4.Challenge/CodeLou.CSharp.Week4.Challenge/Controllers/DefaultController.cs
+++ Challenges/Week4/CodeLou.CSharp.Week4.Challenge/CodeLou.CSharp.Week4.Challenge/Controllers/DefaultController.cs
@@ -23,7 +23,9 @@ namespace CodeLou.CSharp.Week4.Challenge.Controllers
         // GET: Default
         public ActionResult Index(string OrderBy)
         {
-            // I am going to add my SQL directly here for the demonstration, however it is not good practice to do so. It definitely violates separation of concerns
+            // new up a repository class so you can start using data, since we're using SQL Repository, pass in either the
+            // local file connection string or MsSql connection string, as the libraries they use are the same.
+            SqlRepository repository = new SqlRepository(_LocalFileConnectionString);
 
             // Create a list of employees to return to the view based on our SQL statement
 
@@ -38,10 +40,9 @@ namespace CodeLou.CSharp.Week4.Challenge.Controllers
             // TODO: How to we order the data by a column, enable sorting?
             if (!String.IsNullOrEmpty(OrderBy))
             {
-                
             }
 
-            List<Employee> allEmployees = getEmployees(sql);
+            List<Employee> allEmployees = repository.GetEmployees(sql);
             return View(allEmployees);
         }
         // GET: Detail
@@ -87,96 +88,6 @@ namespace CodeLou.CSharp.Week4.Challenge.Controllers
         {
             // TODO: Create employee from form submission, redirect to list
             return View();
-        }
-
-        private void deleteEmplyee(int Id)
-        { 
-            // TODO: Delete Employee based on the Id
-        }
-        private void createEmployee(Employee employee)
-        { 
-            // TODO: Create an employee based on the model
-        }
-        private Employee getOneEmployee(int Id)
-        {
-            // TODO: Get only 1 employee from the database matching the Id passed in.
-            return null;
-        }
-        private List<Employee> getEmployees(string Where)
-        {
-            // Create a list of employees to return to the view
-            List<Employee> allEmployees = new List<Employee>();
-
-            // Connection to your SQL server, MySql, MsSql, Local MDF File
-            using (SqlConnection connection = new SqlConnection(_LocalFileConnectionString))
-            {
-                // The SQL command you want to run on your server to get data back
-                using (SqlCommand command = new SqlCommand(Where, connection))
-                {
-                    // The adapter that is responsible for connecting to the database and getting the data
-                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
-                    {
-                        // This is the variable that will contain all of your data returned to you
-                        DataSet dataSet = new DataSet();
-
-                        // Fill the dataset
-                        adapter.Fill(dataSet);
-
-                        // First lets check to see if we have any data
-                        if (dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
-                        {
-                            // If we have data, now we have to convert it into a View Model, so MVC can create our view. Since
-                            // you can make a view strongly typed (meaning you can only pass a certain view model to it) we cannot
-                            // pass the raw Table to the view.
-
-                            // loop through all the rows
-                            foreach (DataRow row in dataSet.Tables[0].Rows)
-                            {
-                                // new Employee
-                                // the column names are the same as our properties for ease of use
-                                // it's best to do this so you don't have to guess what property maps to what column
-
-                                // also be sure to match up the datatypes from SQL. You'll have to convert INT and Dates etc.
-                                Employee employee = new Employee();
-                                employee.ActiveEmployee = Convert.ToBoolean(row["ActiveEmployee"]);
-                                employee.DepartmentId = Convert.ToInt32(row["DepartmentId"]);
-                                employee.EMail = row["EMail"].ToString();
-                                employee.Extension = row["Extension"].ToString();
-                                employee.FirstName = row["FirstName"].ToString();
-                                employee.LastName = row["LastName"].ToString();
-                                employee.HireDate = Convert.ToDateTime(row["HireDate"]);
-                                employee.Id = Convert.ToInt32(row["Id"]);
-                                employee.Phone = row["Phone"].ToString();
-                                employee.PositionId = Convert.ToInt32(row["PositionId"]);
-                                employee.StartTime = row["StartTime"].ToString();
-
-                                // be sure to check for null values as well
-                                if (row["TerminationDate"] != DBNull.Value)
-                                {
-                                    employee.TerminationDate = Convert.ToDateTime(row["TerminationDate"]);
-                                }
-
-                                // is the table joined?, remember relational databased store additional data in other tables. If we join
-                                // our employee on the Department table and Position table then we get that information to display
-                                if (row["DepartmentName"] != DBNull.Value)
-                                {
-                                    employee.DepartmentName = row["DepartmentName"].ToString();
-                                }
-
-                                if (row["PositionName"] != DBNull.Value)
-                                {
-                                    employee.PositionName = row["PositionName"].ToString();
-                                }
-
-                                // add the employee to the list of employees
-                                allEmployees.Add(employee);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return allEmployees;
-        }
+        }        
     }    
 }
