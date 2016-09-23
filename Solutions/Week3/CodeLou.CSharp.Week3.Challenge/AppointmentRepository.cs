9913b37b13c8e588af6dcfac274ea9f8f3a48859
Charles D Waddell <cdwaddell@outlook.com>
2016-09-23 14:21:06 -0400

    Solution to Week 3

---
+++ Solutions/Week3/CodeLou.CSharp.Week3.Challenge/AppointmentRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeLou.CSharp.Week3.Challenge
+{
+	public class AppointmentRepository: CalendarItemRepositoryBase, ICalendarItemRepository<Appointment>
+	{
+		public Appointment Create()
+		{
+			return base.Create<Appointment>();
+		}
+		
+		public new Appointment FindById(int id)
+		{
+			return (Appointment)base.FindById(id);
+		}
+
+		public Appointment Update(Appointment item)
+		{
+			return (Appointment) base.Update(item);
+		}
+
+		public void Delete(Appointment item)
+		{
+			DeleteById(item.Id);
+		}
+
+		public IEnumerable<Appointment> FindByDate(DateTime date)
+		{
+			return GetAllItems()
+				.Where(item => item.StartDateTime >= date.Date && date.Date <= item.EndDateTime);
+		}
+
+		public IEnumerable<Appointment> GetAllItems()
+		{
+			return Dictionary.Values.Cast<Appointment>();
+		}
+	}
+}
---
+++ Solutions/Week3/CodeLou.CSharp.Week3.Challenge/ReminderRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeLou.CSharp.Week3.Challenge
+{
+	public class ReminderRepository: CalendarItemRepositoryBase, ICalendarItemRepository<Reminder>
+	{
+		public Reminder Create()
+		{
+			return base.Create<Reminder>();
+		}
+
+		public new Reminder FindById(int id)
+		{
+			return (Reminder)base.FindById(id);
+		}
+
+		public Reminder Update(Reminder item)
+		{
+			return (Reminder)base.Update(item);
+		}
+
+		public void Delete(Reminder item)
+		{
+			DeleteById(item.Id);
+		}
+
+		public IEnumerable<Reminder> GetAllItems()
+		{
+			return Dictionary.Values.Cast<Reminder>();
+		}
+
+		public IEnumerable<Reminder> FindByDate(DateTime date)
+		{
+			return GetAllItems()
+				.Where(item => item.StartDateTime.Date == date.Date);
+		}
+	}
+}
